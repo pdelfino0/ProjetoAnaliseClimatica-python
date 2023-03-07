fe715e708d6458b3310e72c443f25952c26a3888
Pedro Delfino <pedrofernandesdelfino@gmail.com>
2023-03-07 00:53:00 -0300

    first commit

---
+++ Program.py
@@ -0,0 +1,46 @@
+def load_data(file_name):
+    with open(file_name) as f:
+        f.readline()  # ignorar a primeira linha
+        data = [line.strip().split(";") for line in f]
+        return data
+
+
+def is_leap_year(year):
+    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+        return True
+    else:
+        return False
+
+
+data = load_data("ArquivoDadosProjeto.csv")
+
+day_list = []
+for line in data:
+    informations = (line[1], line[2], line[3],
+                    line[4], line[5], line[6], line[7])
+    day = [line[0], informations]
+    day_list.append(day)
+'''
+for i in range(len(day_list)):
+    print((day_list[i][0][6:]))'''
+
+#print(teste)
+#print(len(day_list[1]))
+
+
+def seach_precipitation( year, month):
+    result_list = []
+    for i in range(len(day_list)):
+        if (day_list[i][0][6:]) == str(year):
+            #print((day_list[i][0][6:]))
+            result_list.append((day_list[i]))
+    return result_list
+            
+'''def printDays(list):
+    for day in day_list:
+        print(day)
+
+printDays(day_list)'''
+
+teste = seach_precipitation(1961, 1)
+print(teste)
