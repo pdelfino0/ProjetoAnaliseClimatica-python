19e017797a3d6f3edb88f4dfbdb0d8b16e808c5c
Pedro Delfino <pedrofernandesdelfino@gmail.com>
2023-03-14 19:21:42 -0300

    testando

--- testesFase2.py
+++ testesFase2.py
@@ -58,3 +58,41 @@ media_um_relativa = soma_um_relativa / cont
 print(f"{media_temp_min:5.1f}C")
 print(f"{media_vel_vento:5.1f}")
 print(f"{media_um_relativa:5.1f}")
+
+dicionario = {}
+
+anos_decada_1 = ["61","62","63","64","65","66","67","68","69","70"]
+anos_decada_2 = ["71", "72", "73", "74", "75", "76", "77", "78", "79","80"]
+anos_decada_3 = ["81", "82", "83", "84", "85", "86", "87", "88", "89", "90"]
+anos_decada_4 = ["91", "92", "93", "94", "95", "96", "97", "98", "99", "00"]
+anos_decada_5 = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"]
+anos_decada_6 = ["11","12","13","14","15","16"]
+
+for day in day_list:
+    ano = day[0][8:]
+    if ano in anos_decada_1:chave_dicionario = "Decada 1"
+    elif ano in anos_decada_2:chave_dicionario = "Decada 2"
+    elif ano in anos_decada_3:chave_dicionario = "Decada 3"
+    elif ano in anos_decada_4:chave_dicionario = "Decada 4"
+    elif ano in anos_decada_5:chave_dicionario = "Decada 5"
+    else: chave_dicionario = "Decada 6"
+    precipitacao_hoje = float(day[1][0])
+    dicionario[chave_dicionario] = (dicionario.get(chave_dicionario, 0) + precipitacao_hoje)
+    
+
+for chave in dicionario:
+    if chave == "Decada 6": anos = len(anos_decada_6)
+    else: anos = 10
+    dicionario[chave] /= anos
+    dicionario[chave] = round(dicionario[chave],1)
+    
+    
+print(dicionario)
+
+#Década 1: 1961 - 1970
+#Década 2: 1971 - 1980
+#Década 3: 1981 – 1990
+#Década 4: 1991 – 2000
+#Década 5: 2001 – 2010
+#Década 6: 2011 – 2016
+
